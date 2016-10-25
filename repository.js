832ef77ebc75f513727c3de6a283516b6f3fd7cc
Chie <akiba.chie@gmail.com>
2016-10-25 14:39:29 -0700

    Finished assignment

--- repository.js
+++ repository.js
@@ -6,14 +6,16 @@ var StarRater = require('./star-rater');
 var actions = require('./actions');
 
 var Repository = React.createClass({
+  componentDidMount: function() {
+    this.props.dispatch(actions.fetchDescription(this.props.repository.name));
+  },
   changeRating: function(rating) {
     this.props.dispatch(actions.rateRepository(this.props.repository.name, rating));
   },
-
   render: function() {
     return (
       <div className='repository'>
-        {this.props.repository.name}
+        {this.props.repository.name} - {this.props.repository.description}
         &nbsp;
         <StarRater rating={this.props.repository.rating} onChange={this.changeRating}/>
       </div>
